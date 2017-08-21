c7009ccba9e89e741f0042360a32d63cb35baed3
Deathsteps <lovely_dreamer@126.com>
2017-08-21 08:08:40 +0800

    server side, data service

--- build/data-service-router.js
+++ build/data-service-router.js
@@ -1,65 +1,82 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
+var JSONStream = require('JSONStream');
 
 var MongoClient = require('mongodb').MongoClient;
-
 // Connection URL
 var DB_URL = 'mongodb://localhost:27017/shop';
-
-var connectDataBase = (res, callback) => {
+function connectDataBase(res, callback) {
   MongoClient.connect(DB_URL, function(err, db) {
     if (err) {
       res.status(500)
         .send({ error: 'An error happened connecting the database!' })
         .end();
     } else {
-      callback(db)
+      callback(db);
     }
   });
 }
 
+function responseResult(cursor, res, db) {
+  // pipe the json steam to response
+  cursor.stream()
+    .pipe(JSONStream.stringify())
+    .pipe(res.type('json'))
+    .on('end', () => {
+      // close connection and end response
+      db.close();
+      res.end();
+    });
+}
+
+var router = express.Router();
+
 router.post('/categories', function (req, res, next) {
   connectDataBase(res, db => {
     var superId = req.body.superId || 0;
-    var level = req.body.superId;
-    var result =
+    var level = req.body.level;
+    var cursor =
       db.collection('Categories')
         .find({ level: level, superCategoryId: superId });
-    res.json(result).end()
+    responseResult(cursor, res, db);
   })
 })
 router.post('/hot-sale', function (req, res, next) {
   connectDataBase(res, db => {
-    var result =
+    var cursor =
       db.collection('ListProducts')
-        .find({ $in : [1136010, 1110004, 1145021, 1167004, 1167004] });
-    res.json(result).end()
+        .find({ id: { $in : [1136010, 1110004, 1145021, 1167004, 1167004] } });
+    // pipe the json steam to response
+    responseResult(cursor, res, db);
   })
 })
 router.post('/list-products', function (req, res, next) {
   connectDataBase(res, db => {
-    var result =
+    var pageSize = req.body.pageSize
+    var pageIndex = req.body.pageIndex
+    var cursor =
       db.collection('ListProducts')
-        .find({ categoryId: req.body.categoryId });
-    res.json(result).end()
+        .find({ categoryId: req.body.categoryId })
+        .limit( pageSize )
+        .skip( (pageIndex - 1) * pageSize );
+    responseResult(cursor, res, db);
   })
 })
 router.post('/product', function (req, res, next) {
   connectDataBase(res, db => {
-    var result =
+    var cursor =
       db.collection('Products')
         .find({ id: req.body.id });
-    res.json(result).end()
+    responseResult(cursor, res, db);
   })
 })
 router.post('/comments', function (req, res, next) {
   connectDataBase(res, db => {
-    var result =
+    var cursor =
       db.collection('Comments')
         .find({ productId: req.body.productId });
-    res.json(result).end()
+    responseResult(cursor, res, db);
   })
 })
 
---
+++ src/components/IndexPage.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="">
+    <h1>This is IndexPage</h1>
+  </div>
+</template>
+
+<script>
+import { fetchTopCategories, fetchHotSale } from '../api.js'
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      categories: null
+    }
+  },
+  beforeMount () {
+    // TODO: check data existence in fetchHomeProducts
+    if (!this.categories) {
+      fetchTopCategories()
+      fetchHotSale()
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
